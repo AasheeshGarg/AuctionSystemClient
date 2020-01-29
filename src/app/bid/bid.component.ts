80d47705c26e36b40475fde20b794346351e6c8a
AasheeshGarg <aasheeshgarg28@gmail.com>
2020-01-29 07:42:48 -0800

    Add files via upload

--- src/app/bid/bid.component.ts
+++ src/app/bid/bid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-bid',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BidComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sharedServ:SharedService,private route: ActivatedRoute, private router: Router) { }
 
+  public bidValue;
+  
   ngOnInit() {
+  }
+
+  createObject(){
+    let data = {
+      "bidValue" : this.bidValue,
+      "bidStatus" : "CREATED",
+      "date" : 1580151313,
+      "auction" : { "auctionId" : 3 },
+      "user" : { "userId" : 1 }
+    }
+    return data;
+  }
+
+  saveBid() {
+    let data = this.createObject();
+    console.log(data);
+    this.sharedServ.saveBid(data).subscribe((result) => {
+      console.log(result);
+      alert("Bid Successfully Added !!!");
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
